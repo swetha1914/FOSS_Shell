4d400e8a7ee08616f384e9bf5212f3d9967427b0
merothh <merothh@gmail.com>
2019-03-27 13:33:24 +0530

    kill.sh: make it work (#2)

--- kill.sh
+++ kill.sh
@@ -1,25 +1,31 @@
 #!/bin/bash
 
-cpu_limit=20
-mem_limit=10
+cpu_limit=40
+mem_limit=5
 
 while [ 1 ];
 do 
   ps -eo pid,%mem,%cpu,cmd,ppid --sort=-%mem | head > ps.list
-  for line in {1..9}
+  for line in {2..9}
   do 
-	  mem=$(sed -n `echo $line`p ps.list | awk '{print $2}')
+
+    mem=$(sed -n `echo $line`p ps.list | awk '{print $2}')
     cpu=$(sed -n `echo $line`p ps.list | awk '{print $3}')
     pid=$(sed -n `echo $line`p ps.list | awk '{print $1}')
 
-    if [ $(echo "$mem > $mem_limit" |bc -l) ]; then
-      echo -e "$(sed -n `echo $line`p ps.list | awk '{print $4}') memory limit exceeded, killing it"
+    exceeds_limit=$(echo "$cpu > $cpu_limit" | bc -l)
+    if [ "$exceeds_limit"  = "1" ]; then
+	    echo -e "$(sed -n $(echo $line)p ps.list | awk '{print $4}') CPU limit exceeded, killing it\n"
+      kill $pid
+      continue
+    fi
+
+    exceeds_limit=$(echo "$mem > $mem_limit" | bc -l)
+    if [  "$exceeds_limit" = "1" ]; then
+	    echo -e "$(sed -n $(echo $line)p ps.list | awk '{print $4}') memory limit exceeded, killing it\n"
       kill $pid
     fi
 
-    if [ $(echo "$cpu > $cpu_limit" |bc -l) ]; then
-      echo -e "$(sed -n `echo $line`p ps.list | awk '{print $4}') CPU limit exceeded, killing it"
-      kill $pid
-    fi
   done
+
 done
