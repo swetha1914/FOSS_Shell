93a53690447c1669812f60681c71b8ddd10b82c7
AToMiXhawK <githinmanesh@gmail.com>
2019-04-09 09:47:38 +0530

    AWK more examples added

--- expts/9~expt.sh
+++ expts/9~expt.sh
@@ -36,25 +36,25 @@ case $qno in
     ;;
 
     6)
-        echo -e "\nQ5. Names starting with p or ending with a"
+        echo -e "\nQ6. Names starting with p or ending with a"
         echo -e "\nAnswer:\n"
         awk '$1 ~ /^p|a$/ {print $1}' name.txt
     ;;
 
     7)
-        echo -e "\nQ6. Names having age>40"
+        echo -e "\nQ7. Names having age>40"
         echo -e "\nAnswer:\n"
         awk '$2>40 {print $1}' name.txt
     ;;
 
     8)
-        echo -e "\nQ7. Name not starting with p"
+        echo -e "\nQ8. Name not starting with p"
         echo -e "\nAnswer:\n"  
         awk '$1 !~ /^p/ {print $1}' name.txt
     ;;
 
     9)
-        echo -e "\nQ8. Name not starting with p or ending with a"
+        echo -e "\nQ9. Name not starting with p or ending with a"
         echo -e "\nAnswer:\n" 
         awk '$1 !~ /^p|a$/ {print $1}' name.txt
 
@@ -62,16 +62,23 @@ case $qno in
     ;;
 
     10)
-        echo -e "\nQ9. Name starting with p and age>26"
+        echo -e "\nQ10. Name starting with p and age>26"
         echo -e "\nAnswer:\n"
         awk '$1 ~ /^p/ && $2>26 {print $1}' name.txt
 
     ;;
 
     11)
-        echo -e "\nQ10. Name starting with aeiou"
+        echo -e "\nQ11. Name starting with aeiou"
         echo -e "\nAnswer:\n" 
         awk '/^a|^e|^i|^o|^u/ {print $1}' name.txt
+ 
+    ;;
+
+    12)
+        echo -e "\nQ12. Names and length of names"
+        echo -e "\nAnswer:\n" 
+        awk '{print $1, length($1)}' name.txt
  
     ;;
 
